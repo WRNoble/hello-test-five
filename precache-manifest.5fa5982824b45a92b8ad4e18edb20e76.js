b8b80ee1cc3f07fc9d0518ec7504cadc0855dbae
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-08-03 11:39:05 -0400

    Updates

---
+++ precache-manifest.5fa5982824b45a92b8ad4e18edb20e76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b9f55b748110e83c41593658606c823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7452f09847b8239cf4b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4cf5d2c7ec96e2ac844c",
+    "url": "/static/js/2.4076d915.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4076d915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7452f09847b8239cf4b",
+    "url": "/static/js/main.5e6952a8.chunk.js"
+  },
+  {
+    "revision": "c92d64e19835cf6e9f35",
+    "url": "/static/js/runtime-main.03b3320c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
